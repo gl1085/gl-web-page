ef987c047f0beadfeb280c3d69a6eccf254c6ae5
Tey Shu Wen <teyshuwen@Teys-MacBook-Pro.local>
2023-07-28 15:54:50 +0800

    app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
     
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
+          href="https://twitter.com/sgl5308" // Updated Twitter link here
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
