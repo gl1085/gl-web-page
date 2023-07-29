781841b3a9eb19e68db900621e47acfea5bd302d
Tey Shu Wen <teyshuwen@Teys-MacBook-Pro.local>
2023-07-29 13:08:50 +0800

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
   const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
+    "https://api.github.com/repos/sgl5308/gl-web-page",
     {
       ...(process.env.GITHUB_OAUTH_TOKEN && {
         headers: {
@@ -29,7 +29,7 @@ export default async function Home() {
     
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
-          href="https://twitter.com/sgl5308" // Updated Twitter link here
+          href="https://twitter.com/leng77068546" 
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
